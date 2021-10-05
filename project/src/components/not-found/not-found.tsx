b80617190f43389993937f66b22dfaa3059aa99a
Алексей <gris.mego@gmail.com>
2021-10-05 12:50:28 +0700

    routes

--- project/src/components/not-found/not-found.tsx
+++ project/src/components/not-found/not-found.tsx
@@ -1,7 +1,11 @@
+import { Routes } from '../../constants/constants';
+import { Link } from 'react-router-dom';
+
 export function NotFound(): JSX.Element {
   return (
     <section>
       <h1>404: Not Found</h1>
+      <Link to={Routes.MAIN}>Back to main page</Link>
     </section>
   );
 }
---
+++ project/src/constants/constants.ts
@@ -0,0 +1,13 @@
+export enum Routes {
+  MAIN = '/',
+  SIGN_IN = '/login',
+  MY_LIST = '/mylist',
+  FILM = '/films/:id',
+  ADD_REVIEW = '/films/:id/review',
+  PLAYER = '/player/:id',
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NotAuth = 'NOT_AUTH',
+}
