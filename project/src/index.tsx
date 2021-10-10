c65604df5edb470421e7fd175ca24223ebca240a
Алексей <gris.mego@gmail.com>
2021-10-10 23:27:07 +0700

    added state & иÑbinding routes

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,14 +1,14 @@
 import App from './components/app/app';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Movies } from './mocks/movies';
+import { movies } from './mocks/movies';
 import genres from './mocks/genres.json';
 
 ReactDOM.render(
   <React.StrictMode>
     <App
-      movies={Movies}
-      currentMovie={Movies[0]}
+      movies={movies}
+      currentMovie={movies[0]}
       genres={genres}
     />
   </React.StrictMode>,
