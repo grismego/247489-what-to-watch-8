ce4aba901e57610500cb3d82be431df68a26e472
Алексей <gris.mego@gmail.com>
2021-10-25 22:06:00 +0700

    fix

--- project/src/components/video-player/video-player.tsx
+++ project/src/components/video-player/video-player.tsx
@@ -1,5 +1,3 @@
-import { useEffect, useRef, useState } from 'react';
-
 type PropsType = {
   muted?: boolean,
   autoplay?: boolean,
@@ -8,29 +6,12 @@ type PropsType = {
 }
 
 export function VideoPlayer({ autoplay = false, muted = true, src, poster }: PropsType): JSX.Element {
-  const [isPlaying] = useState(autoplay);
-
-  const videoRef = useRef<HTMLVideoElement | null>(null);
-
-  useEffect(() => {
-    if (videoRef.current === null) {
-      return;
-    }
-
-    if (isPlaying) {
-      videoRef.current.play();
-      return;
-    }
-
-    videoRef.current.pause();
-  }, [isPlaying]);
-
   return (
     <video
       src={src}
       poster={poster}
       muted={muted}
-      ref={videoRef}
+      autoPlay={autoplay}
       width="280"
       height="175"
     />
