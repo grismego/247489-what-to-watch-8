bc44d4b736402ef8a0ff1a9272056552398daf77
Aleksey <gris.mego@gmail.com>
2021-10-31 14:04:12 +0700

    redux

--- project/src/store/actions.ts
+++ project/src/store/actions.ts
@@ -1,13 +1,12 @@
-import { GenreType } from '../components/genre/genre';
 import { MoviePropsType } from '../components/movie-card/movie-card';
 import { ActionType } from '../types/actions';
 
-export const changeGenre = (genre: GenreType) => ({
+export const changeGenre = (genre: string) => ({
   type: ActionType.CHANGE_GENRE,
   payload: genre,
 } as const);
 
-export const SetMovies = (movies: MoviePropsType[]) => ({
+export const setMovies = (movies: MoviePropsType[]) => ({
   type: ActionType.SET_MOVIES,
   payload: movies,
 } as const);
